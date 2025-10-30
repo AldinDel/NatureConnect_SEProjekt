afeb5477e7a6d93149dc13de75eeb7a754ce068b
Aldin <aldin.delahmet@gmx.at>
2025-10-30 13:02:40 +0100

    find event position changed to top 6px (style.css)

--- src/main/java/at/fhv/Authors/persistence/AuthorRepository.java
+++ src/main/java/at/fhv/Authors/persistence/AuthorRepository.java
@@ -9,3 +9,5 @@ import java.util.UUID;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
+
+//public interface EventRepository extends JpaRepository<>{}
--- src/main/java/at/fhv/Authors/ui/AuthorTemplateProvider.java
+++ src/main/java/at/fhv/Authors/ui/AuthorTemplateProvider.java
@@ -1,6 +1,8 @@
 package at.fhv.Authors.ui;
 
 import at.fhv.Authors.domain.Author;
+import at.fhv.Authors.persistence.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +14,20 @@ import java.util.List;
 @Controller
 public class AuthorTemplateProvider {
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     @GetMapping("/authors")
     public ModelAndView getAuthorTemplate(Model model) {
         List<Author> authors = Arrays.asList(new Author("Ralph", "Hoch"), new Author("FH", "Vorarlberg"));
         return new ModelAndView("nature_connect", "authors", authors);
     }
 
+    @GetMapping("/events")
+    public ModelAndView showEventList(Model model) {
 
+        return new ModelAndView("events/events_list", "events_list", null);
+    }
 }
 
 
