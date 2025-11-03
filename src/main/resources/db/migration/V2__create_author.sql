d04c0648c84b71dea5663e41ffdc6ce4a45321ad
smy42 <sumeyye.direk42@gmail.com>
2025-11-03 17:29:47 +0100

    fix: restore author table and Flyway migration (app runs successfully again)

---
+++ src/main/resources/db/migration/V2__create_author.sql
@@ -0,0 +1,9 @@
+-- Author, können wir später löschen
+create table if not exists author (
+                                      id          bigserial primary key,
+                                      first_name  varchar(100) not null,
+    last_name   varchar(100) not null,
+    email       varchar(255) not null unique
+    );
+
+
