2fbbeffe3e404836f11bc760827a44e92dd91f37
Begüm Özkaya <boe1301@students.fhv.at>
2025-12-14 17:15:00 +0100

    add invoice repository interface

---
+++ src/main/java/at/fhv/Event/domain/model/invoice/InvoiceRepository.java
@@ -0,0 +1,15 @@
+package at.fhv.Event.domain.model.invoice;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InvoiceRepository {
+
+    Invoice save(Invoice invoice);
+
+    Optional<Invoice> findById(Long id);
+
+    List<Invoice> findByBookingId(Long bookingId);
+
+    List<Invoice> findAll();
+}
