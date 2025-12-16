5796c2c7108efab4114d0995e550dd5094f3dce0
Begüm Özkaya <boe1301@students.fhv.at>
2025-12-16 08:20:40 +0100

    fixed check out bug

--- src/main/java/at/fhv/Event/infrastructure/persistence/booking/JpaBookingParticipantRepository.java
+++ src/main/java/at/fhv/Event/infrastructure/persistence/booking/JpaBookingParticipantRepository.java
@@ -3,15 +3,10 @@ package at.fhv.Event.infrastructure.persistence.booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-import at.fhv.Event.domain.model.booking.ParticipantStatus;
 
 public interface JpaBookingParticipantRepository
         extends JpaRepository<BookingParticipantEntity, Long> {
 
     List<BookingParticipantEntity> findByBookingId(Long bookingId);
 
-    boolean existsByBooking_IdAndCheckOutStatus(
-            Long bookingId,
-            ParticipantStatus checkOutStatus
-    );
 }
